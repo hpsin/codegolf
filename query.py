07b4406b665f4b45b66cde4f64a1465fa377fc60
Hirsch Singhal <hps.scout@gmail.com>
2012-04-23 18:00:09 -0400

    codegolf v1

---
+++ query.py
@@ -0,0 +1,12 @@
+import sys,os,subprocess as v,json
+s=sys.stdout
+def p(l):
+ s.write(l);s.flush()
+t=sys.argv[1]
+while 1:
+ p("\n> ");c=raw_input().split();r=[];a=[]
+ for q in c:
+	r+=v.check_output("sed -n 's/\\(.\\{22\\}\\).*"+q+".*/\\1/p' "+t,shell=True).split(" \n")[:-1]
+ for m in r:
+	if m not in a and r.count(m)==len(c):a+=m,
+ p(json.dumps(a))
