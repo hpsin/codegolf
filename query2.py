f4c5baf9195ef4151f89b4436ef074cc0578780b
Hirsch Singhal <hps.scout@gmail.com>
2012-04-23 21:55:40 -0400

    "sed to grep"

--- query2.py
+++ query2.py
@@ -1,12 +1,12 @@
 import sys,os,subprocess as v,json
 s=sys.stdout
-def p(l):
- s.write(l);s.flush()
+def p(l):s.write(l);s.flush()
 t=sys.argv[1]
 while 1:
  p("\n> ");c=raw_input().split();r=[];a=[]
  for q in c:
-	r+=v.check_output("sed -n 's/\\(.\\{22\\}\\).*"+q+".*/\\1/p' "+t,shell=True).split(" \n")
+	r+=v.check_output("grep '.\\{22\\}.*"+q+".*' "+t,shell=True).split("\n")
  for m in r:
-	if m not in a and r.count(m)==len(c):a+=m,
+	d=m[:21]
+	if d not in a and r.count(m)==len(c):a+=d,
  p(json.dumps(a))
